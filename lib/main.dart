fb91b391775dff95cac96c05573d36fa7eaa124c
gchamounster <gchamounster123@gmail.com>
2018-10-23 16:12:31 -0500

    new ajax

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-Future<Post> fetchPost() async {
-  final response = await http.get(
-    'https://jsonplaceholder.typicode.com/posts/1',
-    headers: {HttpHeaders.authorizationHeader: "Basic your_api_token_here"},
-  );
-  final responseJson = json.decode(response.body);
 
-  return Post.fromJson(responseJson);
-}
-
-class Post {
-  final int userId;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.userId, this.id, this.title, this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
-  }
-}
 
 
 
@@ -50,21 +25,12 @@ class TestApp extends StatelessWidget {
           title: Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Post>(
-            future: fetchPost(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.title);
-              } else if (snapshot.hasError) {
+            Dio dio = new Dio();
 
-                return Text("${snapshot.error}");
-              }
+          Response<String> response=await dio.get("https://www.google.com/");
+        print(response.data);
 
-              // By default, show a loading spinner
-              return CircularProgressIndicator();
-            },
-          ),
-        ),
+    ),
       ),
     );
   }
